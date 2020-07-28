6161f74a25e4c8e5e91e465e4307eeb5d94509a3
ajhanson22227 <ajhanson22227@gmail.com>
2020-07-27 19:38:18 -0500

    create window

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    window.setFramerateLimit(60);
+    
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
